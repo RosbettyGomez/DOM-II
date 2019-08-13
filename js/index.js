9003d5855707bea53902d47f0d818fa8a314cd29
RosbettyGomez <rosbettygomez@gmail.com>
2019-08-13 19:55:00 -0400

    header color change

--- js/index.js
+++ js/index.js
@@ -1,24 +1,36 @@
 // Your code goes here
+
+//document.querySelectorAll will return a Nodelist.
 let nav = document.querySelectorAll("a");//Selected "a" tags and stored them in "nav"/
-Array.from(nav); //Converted "nav" into an array
+Array.from(nav); //Converted "nav" from a Nodelist into an array in order to manipulate each "a" tag
 nav.forEach(navLink =>{ //for each item in array do something...
     navLink.addEventListener('click', (event)=> {
-        navLink.style.color = 'red';
+        navLink.style.color = 'green';
     })
 })
 
 let button = document.querySelectorAll(".btn");
 Array.from(button);
 button.forEach(btn =>{
-    btn.addEventListener('mouseover', (event)=>{
-        btn.style.backgroundColor = 'red';
+    btn.addEventListener('mouseenter', (event)=>{
+        btn.style.backgroundColor = 'beige';
     })
+    btn.addEventListener('mouseleave', (event)=>{
+        btn.style.backgroundColor = 'black';
+     })
 })
+
 
 let foot = document.querySelector(".footer")
 foot.addEventListener('dblclick', (event)=>{
     foot.style.backgroundColor = 'white';
 })
+
+let head = document.querySelector(".main-navigation")
+head.addEventListener('mouseover', (event)=>{
+    head.style.backgroundColor = 'beige';
+})
+
 
 
 
